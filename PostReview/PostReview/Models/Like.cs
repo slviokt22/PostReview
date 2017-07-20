92bf3985e774ec2c6ee468a0fb61a1b3d4de6484
Selvi <Selvi@DESKTOP-CE0PB3C>
2017-07-20 11:08:16 +0700

    edit class like

--- PostReview/PostReview/Models/Like.cs
+++ PostReview/PostReview/Models/Like.cs
@@ -9,7 +9,7 @@ namespace PostReview.Models
     {
         public string email { get; set; }
         public string postid { get; set; }
-        public ApplicationUser user { get; set; }
+        public IndexViewModel user { get; set; }
         public Post post { get; set; }
         public Boolean status { get; set; }
     }
